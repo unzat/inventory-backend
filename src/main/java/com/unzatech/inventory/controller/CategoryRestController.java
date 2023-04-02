56ed1edc30648428dc3a94e0d287179a71525693
Tomas Unzain <tomiunzv@gmail.com>
2023-04-02 09:59:02 -0400

    buscar categoria por id

--- src/main/java/com/unzatech/inventory/controller/CategoryRestController.java
+++ src/main/java/com/unzatech/inventory/controller/CategoryRestController.java
@@ -5,6 +5,7 @@ import com.unzatech.inventory.services.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryRestController {
     @Autowired
     private ICategoryService service;
+    /*
+    * get all the categories
+    * @return
+    * */
     @GetMapping("/categories")
     public ResponseEntity<CategoryResponseRest> searchCategories() {
         ResponseEntity<CategoryResponseRest> response = service.search();
+        return response;
+    }
+    /*
+    * get categories by id
+    * @param id
+    * @return
+    * */
+    @GetMapping("/categories/{id}")
+    public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
+        ResponseEntity<CategoryResponseRest> response = service.searchById(id);
         return response;
     }
 }
