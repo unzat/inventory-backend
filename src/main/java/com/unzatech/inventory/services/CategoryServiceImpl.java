dd632718999a201f937a4dd67f163bc8fae66321
Tomas Unzain <tomiunzv@gmail.com>
2023-04-02 11:29:55 -0400

    servicio eliminar categoria

--- src/main/java/com/unzatech/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/unzatech/inventory/services/CategoryServiceImpl.java
@@ -111,4 +111,19 @@ public class CategoryServiceImpl implements ICategoryService{
         }
         return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
     }
+
+    @Override
+    @Transactional
+    public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+        CategoryResponseRest response = new CategoryResponseRest();
+        try {
+            categoryDao.deleteById(id);
+            response.setMetadata("Respuesta ok","00","Categoria eliminada");
+        }catch (Exception e){
+            response.setMetadata("Respuesta no ok","-1","Error al eliminar categoria");
+            e.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    }
 }
